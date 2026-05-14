4af748e3a6804102cd7f3820adaea001457ffe29
simy46 <missipsa.annane@polymtl.ca>
2026-05-14 10:25:33 +0200

    Add consts.js and main.js for data path constants and entry point setup

---
+++ assets/js/consts.js
@@ -0,0 +1,2 @@
+export const CONSUMER_DATA_PATH = "assets/data/consumer_behavior.json"
+export const DRINKS_SHOP_DATA_PATH = "assets/data/drink_shops.json"
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import { CONSUMER_DATA_PATH, DRINKS_SHOP_DATA_PATH } from "./consts"
+
+
+// entry point
+document.addEventListener('DOMContentLoaded', () => {
+
+})
